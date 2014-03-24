15a86d7cf1b7b59f88e61724abd821298b300afa
Osama Khalid <osamak@gnu.org>
2014-03-24 04:42:03 +0300

    Update activity urls.

--- activities/urls.py
+++ activities/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     url(r'^(?P<activity_id>\d+)/$', views.show, name='show'),
     url(r'^(?P<activity_id>\d+)/edit/$', views.edit, name='edit'),
     url(r'^(?P<activity_id>\d+)/review/$', views.review, name='review'),
+    url(r'^(?P<activity_id>\d+)/participate/$', views.participate, name='participate'),
+    url(r'^(?P<activity_id>\d+)/view_participation/$', views.view_participation, name='view_participation'),
 )

e645a4acfdba33199e79cf9784b37cb75b35ab62
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-03-24 06:19:02 +0300

    Merge branch 'master' of https://github.com/osamak/student-portal

--- activities/models.py
+++ activities/models.py
@@ -3,15 +3,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Activity(models.Model):
-    clubs = models.ManyToManyField('clubs.Club',
-                                   verbose_name=u"النوادي")
+    # For now, we will follow the current practice: only one club will
+    # be considered the primary orginzier.  Others will be cosidered
+    # secondary.
+    #clubs = models.ManyToManyField('clubs.Club',
+    #                               verbose_name=u"النوادي")
+    primary_club = models.ForeignKey('clubs.Club', null=True,
+                                     on_delete=models.SET_NULL,
+                                     related_name='primary_activity',
+                                     verbose_name=u"النادي المنظم")
+    secondary_clubs = models.ManyToManyField('clubs.Club', blank=True,
+                                            null=True,
+                                            related_name="secondary_activity",
+                                            verbose_name=u"النوادي المساندة")
     name = models.CharField(max_length=200, verbose_name=u"الاسم")
     description = models.TextField(verbose_name=u"الوصف")
     requirements = models.TextField(blank=True,
                                     verbose_name=u"المتطلبات")
     submitter = models.ForeignKey(User, null=True,
                                   on_delete=models.SET_NULL)
-    submission_date = models.DateTimeField('date submitted')
+    submission_date = models.DateTimeField('date submitted',
+                                           auto_now_add=True)
+    date = models.DateTimeField('date')
     edit_date = models.DateTimeField('date edited', auto_now=True)
     approval_date = models.DateTimeField(null=True,
                                          verbose_name=u"تاريخ الاعتماد")
@@ -33,9 +46,49 @@ class Activity(models.Model):
     class Meta:
         permissions = (
             ("view_activity", "Can view all available activities."),
-            ("review_activity", "Can review all available activities."),
         )
 
 
     def __unicode__(self):
         return str(self.id)
+
+class Review(models.Model):
+    activity = models.OneToOneField(Activity, verbose_name=u" النشاط")
+    reviewer = models.ForeignKey(User, null=True,
+                                 on_delete=models.SET_NULL)
+    review_date = models.DateTimeField('date reviewed', auto_now_add=True)
+    clubs_notes = models.TextField(blank=True,
+                                   verbose_name=u"ملاحظات على الأندية")
+    name_notes = models.TextField(blank=True,
+                                  verbose_name=u"ملاحظات على الاسم")
+    description_notes = models.TextField(blank=True,
+                                        verbose_name=u"ملاحظات على الوصف")
+    requirement_notes = models.TextField(blank=True,
+                                        verbose_name=u"ملاحظات على المتطلبات")
+    inside_notes = models.TextField(blank=True,
+                                            verbose_name=u"ملاحظات المتعاونون من داخل الجامعة")
+    outside_notes = models.TextField(blank=True,
+                                             verbose_name=u"ملاحظات المتعاونون من خارج الجامعة")
+    participants_notes = models.TextField(blank=True,
+                                          verbose_name=u"ملاحظات على عدد المشاركين")
+    organizers_notes = models.TextField(blank=True,
+                                        verbose_name=u"ملاحظات على عدد المنظمين")
+    class Meta:
+        permissions = (
+            ("view_review", "Can view all available activities."),
+        )
+
+    def __unicode__(self):
+        return str(self.id)
+
+
+class Participation(models.Model):
+    activity = models.OneToOneField(Activity, verbose_name=u" طاشنلا")
+    user = models.ForeignKey(User, null=True,
+                             on_delete=models.SET_NULL)
+    submission_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = (
+            ("view_participation", "Can view all available participations."),
+        )
--- activities/views.py
+++ activities/views.py
@@ -1,20 +1,29 @@
-from datetime import datetime
-
-from django.contrib.auth.decorators import permission_required, user_passes_test
+from django.contrib.auth.decorators import permission_required, login_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.forms import ModelForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
 from django.core.exceptions import PermissionDenied
 
-from activities.models import Activity
+from activities.models import Activity, Review, Participation
+
 
 class ActivityForm(ModelForm):
     class Meta:
         model = Activity
-        fields = ['clubs','name','description', 'participants',
-                  'organizers', 'requirements', 'inside_collaborators',
-                  'outside_collaborators', 'collect_participants']
+        fields = ['primary_club', 'secondary_clubs',
+                  'name','description', 'date', 'participants',
+                  'organizers', 'requirements',
+                  'inside_collaborators', 'outside_collaborators',
+                  'collect_participants']
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['clubs_notes', 'name_notes', 'description_notes',
+                  'requirement_notes', 'inside_notes', 'outside_notes',
+                  'participants_notes', 'organizers_notes']
 
 def list(request):
     # If the user is part of the head of the Student Club, or part of
@@ -22,32 +31,64 @@ def list(request):
     # (i.e. approved, rejected and pending).  Otherwise, a user should
     # only see approved activities and the activities of the clubs
     # they have memberships in (regardless of their status).
-
     if request.user.has_perm('activities.view_activity'):
-        latest_activities = Activity.objects.order_by('-submission_date')[:5]
+        if request.GET.get('pending') == "1":
+            activities = Activity.objects.filter(is_approved=None)
+        else:
+            activities = Activity.objects.all()
     else:
         approved_activities = Activity.objects.filter(is_approved=True)
         if request.user.is_authenticated():
             user_activities = request.user.activity_set.all()
             user_clubs = request.user.memberships.all() | request.user.coordination.all()
-            club_activities = Activity.objects.filter(clubs__in=user_clubs)
+            primary_club_activities = Activity.objects.filter(
+                primary_club__in=user_clubs)
+            secondary_club_activities = Activity.objects.filter(
+                secondary_clubs__in=user_clubs)
         else:
             user_activities = Activity.objects.none()
-            club_activities = Activity.objects.none()
-        activities = approved_activities | user_activities | club_activities
-        latest_activities = activities.order_by('-submission_date')[:5]
+            primary_activities = Activity.objects.none()
+            secondary_club_activities = Activity.objects.none()
+        activities = approved_activities | user_activities | \
+                     primary_activities | secondary_club_activities
 
-    context = {'latest_activities': latest_activities}
+    order = request.GET.get('order')
+    if order == 'date':
+        sorted_activities = activities.order_by('-date')
+    elif order == 'club':
+        sorted_activities = activities.order_by('-primary_club')
+    else:
+        sorted_activities = activities.order_by('-submission_date')
+
+    #Each page of results should have a maximum of 25 activities.
+    paginator = Paginator(sorted_activities, 25)
+    page = request.GET.get('page')
+
+    try:
+        page_activities = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        page_activities = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        page_activities = paginator.page(paginator.num_pages)
+
+    context = {'page_activities': page_activities}
     return render(request, 'activities/list.html', context)
 
 def show(request, activity_id):
     # If the activity is approved, everyone can see it.  If it is not,
     # only the head of the Student Club, the Media Team, the members
-    # of the related clubs and the person who submitted it can see it
+    # of the related clubs and the person who submitted it can see it.
 
     activity = get_object_or_404(Activity, pk=activity_id)
-    user_clubs = request.user.memberships.all() | request.user.coordination.all()
-    activity_clubs = activity.clubs.all()
+    if request.user.is_authenticated():
+        user_clubs = request.user.memberships.all() | request.user.coordination.all()
+    else:
+        user_clubs = Activity.objects.none()
+    activity_primary_club = activity.primary_club
+    activity_secondary_clubs = activity.secondary_clubs.all()
+    activity_clubs = [activity_primary_club] + [club for club in activity_secondary_clubs]
 
     # The third test condition, that is:
     #   any([club in activity_clubs for club in user_clubs])
@@ -61,20 +102,25 @@ def show(request, activity_id):
        not any([club in activity_clubs for club in user_clubs]) and \
        not request.user == activity.submitter:
         raise PermissionDenied
-    context = {'activity': activity}
+
+    can_edit = request.user.has_perm('activities.change_activity') or \
+               activity.primary_club in request.user.coordination.all()
+    can_view_participation = request.user.has_perm('activities.view_participation') or \
+                             activity.primary_club in request.user.coordination.all()
+
+    context = {'activity': activity, 'can_edit': can_edit,
+               'can_view_participation': can_view_participation}
     return render(request, 'activities/show.html', context)
 
 @permission_required('activities.add_activity', raise_exception=True)
 def create(request):
     if request.method == 'POST':
         # Set the submission_date automatically.
-        activity = Activity(submission_date=datetime.now(),
-                            submitter=request.user)
+        activity = Activity(submitter=request.user)
         form = ActivityForm(request.POST, instance=activity)
         if form.is_valid():
-            print form.cleaned_data
             form.save()
-            return HttpResponseRedirect(reverse('activities:index'))
+            return HttpResponseRedirect(reverse('activities:list'))
         else:
             context = {'form': form}
             return render(request, 'activities/new.html', context)
@@ -86,7 +132,9 @@ def create(request):
 def edit(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
     user_coordination = request.user.coordination.all()
-    activity_clubs = activity.clubs.all()
+    activity_primary_club = activity.primary_club
+    activity_secondary_clubs = activity.secondary_clubs.all()
+    activity_clubs = [activity_primary_club] + activity_secondary_clubs
 
     # If the user is neither the submitter, nor has the permission to
     # change activities (i.e. not part of the head of the Student
@@ -100,13 +148,13 @@ def edit(request, activity_id):
     if request.method == 'POST':        
         modified_activity = ActivityForm(request.POST, instance=activity)
         modified_activity.save()
-        return HttpResponseRedirect(reverse('activities:index'))
+        return HttpResponseRedirect(reverse('activities:list'))
     else:
         form = ActivityForm(instance=activity)
         context = {'form': form, 'activity_id': activity_id}
         return render(request, 'activities/new.html', context)
 
-@permission_required('activities.review_activity', raise_exception=True)
+@permission_required('activities.add_review', raise_exception=True)
 def review(request, activity_id):
     if request.method == 'POST':
         activity = get_object_or_404(Activity, pk=activity_id)
@@ -122,5 +170,39 @@ def review(request, activity_id):
             return HttpResponse("You are reviewing activity #%s." % activity_id)
     else:
         activity = get_object_or_404(Activity, pk=activity_id)
-        context = {'activity': activity, 'review': True}
+        review = Review(reviewer=request.user, activity=activity)
+        review_form = ReviewForm(instance=review)
+        context = {'activity': activity, 'is_review': True,
+                   'review_form': review_form}
         return render(request, 'activities/show.html', context)
+
+@login_required
+def participate(request, activity_id):
+    activity = get_object_or_404(Activity, pk=activity_id)
+    existing_participation = Participation.objects.filter(activity=activity,
+                                                       user=request.user)
+    context = {'activity': activity}
+
+    if existing_participation:
+        context['error_message'] = 'already_applied'
+        return render(request, 'activities/participate.html', context)
+        
+    if request.method == 'POST':
+        if request.POST['status'] == '1':
+            Participation.objects.create(activity=activity, user=request.user)
+            return HttpResponseRedirect(reverse('activities:show', args=(activity_id,)))
+        else:
+            context['error_message'] = 'unknown'
+            return render(request, 'activities/participate.html', context)
+    else:
+        return render(request, 'activities/participate.html', context)
+
+def view_participation(request, activity_id):
+    activity = get_object_or_404(Activity, pk=activity_id)
+    if not activity.primary_club in request.user.coordination.all() and \
+       not request.user.has_perm('activities.view_participation'):
+        raise PermissionDenied
+
+    participations = Participation.objects.filter(activity=activity)
+    context = {'participations': participations, 'activity': activity}
+    return render(request, 'activities/view_participations.html', context)

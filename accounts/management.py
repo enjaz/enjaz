163ae0d10dbf0399e9f9d43dcc4d245d21b9ee3e
Osama Khalid <osamak@gnu.org>
2014-06-27 14:18:48 +0300

    More permissions to the presidency group.

--- accounts/management.py
+++ accounts/management.py
@@ -11,21 +11,32 @@ def create_groups(sender, **kwargs):
     view_deanship_review = Permission.objects.get(codename='view_deanship_review')
     deanship_group.permissions.add(view_deanship_review)
     deanship_group.save()
+
     # This group is meant for the President of the Studnet Club and
     # his deputies.
     presidency_group = Group.objects.create(name='presidency')
     add_activity = Permission.objects.get(codename='add_activity')
+    directly_add_activity = Permission.objects.get(codename='directly_add_activity')
     # The 'change_activity' permission indicates that the user can
     # changes activities regardless of the value of is_editable.
     change_activity = Permission.objects.get(codename='change_activity')
+    # The 'view_activity' permission indicates that thie user can view
+    # activities regardless of whether or not they have been approved.
+    view_activity = Permission.objects.get(codename='view_activity')
+    view_participation = Permission.objects.get(codename='view_participation')
     add_club = Permission.objects.get(codename='add_club')
+    change_club = Permission.objects.get(codename='change_club')
+    change_book = Permission.objects.get(codename='change_book')
+    delete_book = Permission.objects.get(codename='delete_book')
     view_presidency_review = Permission.objects.get(codename='view_presidency_review')
     add_presidency_review = Permission.objects.get(codename='add_presidency_review')
-    directly_add_activity = Permission.objects.get(codename='directly_add_activity')
-    presidency_group.permissions.add(add_activity, add_club,
-                                     change_activity,
+    presidency_group.permissions.add(add_activity,
+                                     directly_add_activity,
+                                     change_activity, view_activity,
+                                     view_participation, add_club,
+                                     change_club, change_book,
                                      view_presidency_review,
-                                     add_presidency_review,
-                                     directly_add_activity)
-
+                                     view_deanship_review,
+                                     add_presidency_review)
+    presidency_group.save()
 post_syncdb.connect(create_groups, sender=accounts.models)

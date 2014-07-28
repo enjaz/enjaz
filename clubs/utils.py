963078f5524bed68c9560bd1530ccf1613f6555c
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-07-28 13:34:18 +0300

    Club utility functions

---
+++ clubs/utils.py
@@ -0,0 +1,25 @@
+"""Utility functions related to the clubs app."""
+from .models import Club
+
+
+def is_coordinator(club, user):
+    """Return whether the user is the coordinator of a given club."""
+    return club.coordinator == user
+
+
+def is_member(club, user):
+    """Return whether the user is a member of a given club."""
+    return user in club.members.all()
+
+
+def is_coordinator_or_member(club, user):
+    """Return whether the user is the coordinator or a member of a given club."""
+    return is_coordinator(club, user) or is_member(club, user)
+
+
+def get_presidency():
+    return Club.objects.get(english_name="Presidency")
+
+
+def get_media_center():
+    return Club.objects.get(english_name="Media Center")

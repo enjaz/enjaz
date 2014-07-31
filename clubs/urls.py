097bd2bcde4e48b6e79ac587ecd7c6ebd3e9ba39
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-07-31 18:12:20 +0300

    A cleaner url for the club view_applications view

--- clubs/urls.py
+++ clubs/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     url(r'^(?P<club_id>\d+)/edit/$', views.edit, name='edit'),
     url(r'^(?P<club_id>\d+)/join/$', views.join, name='join'),
     url(r'^(?P<club_id>\d+)/join/done/$', TemplateView.as_view(template_name='clubs/join_done.html'), name='join_done'),
-    url(r'^(?P<club_id>\d+)/view_application/$', views.view_application, name='view_application'),
-    url(r'^(?P<club_id>\d+)/view_application/download$', views.download_application, name='download_application'),
+    url(r'^(?P<club_id>\d+)/applications/$', views.view_application, name='view_application'),
+    url(r'^(?P<club_id>\d+)/applications/download$', views.download_application, name='download_application'),
 )

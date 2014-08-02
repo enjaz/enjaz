307a3a8fddddfd499396c125a3754ffbe712e1cb
Osama Khalid <osamak@gnu.org>
2014-08-02 09:07:23 +0300

    Improve Arshidni admin interface based on Dalal's feedback.

--- arshidni/admin.py
+++ arshidni/admin.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User, Group
 from django.contrib.admin.sites import AdminSite
 from django.core.exceptions import ObjectDoesNotExist
 
-from arshidni.models import Question, Answer, StudyGroup, LearningObjective, group_status_choices, ColleagueProfile, GraduateProfile
+from arshidni.models import Question, Answer, StudyGroup, LearningObjective, group_status_choices, ColleagueProfile, SupervisionRequest, GraduateProfile, supervision_request_status_choices
 
 class ArshidniAuthenticationForm(admin.forms.AdminAuthenticationForm):
     """A custom authentication form used in the admin app.  Based on the
@@ -62,6 +62,30 @@ class GroupStatusFilter(admin.SimpleListFilter):
         if self.value():
             return queryset.filter(status=self.value())
 
+class SupervisionRequestStatusFilter(admin.SimpleListFilter):
+    title = u"حالة الطلب"
+    parameter_name = 'request_status'
+
+    def lookups(self, request, model_admin):
+        return supervision_request_status_choices
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(status=self.value())
+
+class ColleagueAvailabilityFilter(admin.SimpleListFilter):
+    title = u"توفر الزميل"
+    parameter_name = 'available'
+    def lookups(self, request, model_admin):
+        return (
+            (0, u'غير متوفر'),
+            (1, u'متوفر'),)
+
+    def queryset(self, request, queryset):
+        if self.value() == '0':
+            return queryset.filter(is_available=False)
+        elif self.value() == '1':
+            return queryset.filter(is_available=True)
+
 class PublishedFilter(admin.SimpleListFilter):
     title = u"النشر"
     parameter_name = 'published'
@@ -87,6 +111,9 @@ class StudyGroupAdmin(admin.ModelAdmin):
     list_filter = [GroupStatusFilter]
     inlines = [LearningObjectiveInline]
 
+class LearningObjectiveAdmin(admin.ModelAdmin):
+    list_display = ('group', 'is_done', 'submission_date')
+
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('get_title', 'submitter', 'is_answered',
                     'is_published', 'submission_date')
@@ -94,11 +121,27 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [AnswerInline]
 
 class ColleagueProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'batch',)
+    list_display = ('get_user_full_name', 'batch', 'is_available')
+    list_filter = [ColleagueAvailabilityFilter]
+
+    def get_user_full_name(self, obj):
+        return obj.user.student_profile.get_ar_full_name()
+    get_user_full_name.short_description = u"اسم الزميل الطلابي"
 
 class GraduateProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'answers_questions', 'gives_lectures')
 
+class SupervisionRequestAdmin(admin.ModelAdmin):
+    list_display = ('get_user_full_name', 'batch', 'get_colleague_full_name', 'get_full_status', 'submission_date')
+    list_filter = [SupervisionRequestStatusFilter]
+
+    def get_user_full_name(self, obj):
+        return obj.user.student_profile.get_ar_full_name()
+    get_user_full_name.short_description = u"اسم الطالب المستجد"
+
+    def get_colleague_full_name(self, obj):
+        return obj.colleague.user.student_profile.get_ar_full_name()
+    get_colleague_full_name.short_description = u"اسم الزميل الطلابي"
 
 arshidni_admin = ArshidniAdmin("Arshidni Admin")
 
@@ -108,9 +151,15 @@ arshidni_admin.register(StudyGroup, StudyGroupAdmin)
 admin.site.register(Question, QuestionAdmin)
 arshidni_admin.register(Question, QuestionAdmin)
 
+admin.site.register(LearningObjective, LearningObjectiveAdmin)
+arshidni_admin.register(LearningObjective, LearningObjectiveAdmin)
+
 admin.site.register(ColleagueProfile, ColleagueProfileAdmin)
 arshidni_admin.register(ColleagueProfile, ColleagueProfileAdmin)
 
+admin.site.register(SupervisionRequest, SupervisionRequestAdmin)
+arshidni_admin.register(SupervisionRequest, SupervisionRequestAdmin)
 
 admin.site.register(GraduateProfile, GraduateProfileAdmin)
 arshidni_admin.register(GraduateProfile, GraduateProfileAdmin)
+

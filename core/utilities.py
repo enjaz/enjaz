2c40ca4b2d4c22313e7fe7d2377da11e300dcff7
Osama Khalid <osamak@gnu.org>
2014-08-03 01:14:35 +0300

    Settings correction.

--- core/utilities.py
+++ core/utilities.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 
-FVP_EMAIL = User.objects.get(username=settings.FPV_USERNAME).email
-MVP_EMAIL = User.objects.get(username=settings.MPV_USERNAME).email
+FVP_EMAIL = User.objects.get(username=settings.FVP_USERNAME).email
+MVP_EMAIL = User.objects.get(username=settings.MVP_USERNAME).email
 DHA_EMAIL = User.objects.get(username=settings.DHA_USERNAME).email

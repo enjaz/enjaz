1298b0be62e66117101cfc2b4522462f454e0a42
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-08-04 09:22:53 +0300

    Added a utils file to activities, which contains a get_approved_activities utility function

---
+++ activities/utils.py
@@ -0,0 +1,13 @@
+"""
+Utility functions for the activities app.
+"""
+from activities.models import Activity
+
+
+def get_approved_activities():
+    """
+    Return a query set of the current year's approved activities.
+    """
+    # Approved activities are those who have an approved deanship review
+    # TODO: Test (exhaustively)
+    return Activity.objects.filter(review__review_type="D", review__is_approved=True)

d6ef91b6b855c149a44b54a5f88bb873de6b9cd9
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-08-12 10:30:55 +0300

    Tests for list_activities view New test utils add_presidency_review and add_deanship_review

--- activities/tests.py
+++ activities/tests.py
@@ -3,6 +3,6 @@
 Tests for the activities app.
 """
 
-from .test_suites.view_tests import ShowActivityViewTests, ReviewViewTests
-from .test_suites.activity_form_tests import EpisodeTests
-from .test_suites.utilities_tests import ActivityGetterTests
+from .test_suites.view_tests import *
+from .test_suites.activity_form_tests import *
+from .test_suites.utilities_tests import *

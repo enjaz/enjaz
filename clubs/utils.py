066a0888b5a80af952ee50307be4bfdfc1326af5
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-08-13 22:05:50 +0300

    Added utility functions for employees

--- clubs/utils.py
+++ clubs/utils.py
@@ -11,6 +11,9 @@ def is_member_of_any_club(user):
     """Return whether the user is a member of any club."""
     return any([user in club.members.all() for club in Club.objects.all()])
 
+def is_employee_of_any_club(user):
+    """Return whether the user is an employee of any club."""
+    return any([is_employee(club, user) for club in Club.objects.all()])
 
 def is_coordinator(club, user):
     """Return whether the user is the coordinator of a given club."""
@@ -26,6 +29,9 @@ def is_coordinator_or_member(club, user):
     """Return whether the user is the coordinator or a member of a given club."""
     return is_coordinator(club, user) or is_member(club, user)
 
+def is_employee(club, user):
+    """Return whether the user is the employee assigned to a given club."""
+    return user == club.employee
 
 def get_presidency():
     return Club.objects.get(english_name="Presidency")

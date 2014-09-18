f5fa9da6d3b663b52ea1548a7f26944f99a2c6b0
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-09-18 17:49:12 +0300

    More readability for printed messages

--- niqati/management/commands/generateniqati.py
+++ niqati/management/commands/generateniqati.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from niqati.models import Code_Order
 import os
 
@@ -10,6 +11,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Get all approved and unprocessed orders
         orders = filter(lambda order: order.is_approved() and order.is_processed() == False, Code_Order.objects.all())
+        print timezone.now()
         # Process the orders one at a time so take only the first order
         if len(orders) > 0:
             order = orders.pop(0)
@@ -20,22 +22,24 @@ class Command(BaseCommand):
             if order.get_delivery_type() == '0':  # Coupon
                 # Check if no other coupons are already being generated at the moment
                 if not os.path.isfile('codes.pdf'):
-                    print "No coupons currently being generated."
-                    print "Processing order number ", order.pk, " for the activity ", order.activity
+                    print "  No coupons currently being generated."
+                    print "  Processing order number ", order.pk, " for the activity ", order.activity
                     order.process(submit_link)
-                    print "Successfully generated requested coupons."
+                    print timezone.now()
+                    print "  Successfully generated requested coupons."
                 else:
-                    print "Other codes are currently being generated"
-                    print "Aborting..."
+                    print "  Other codes are currently being generated"
+                    print "  Aborting..."
             elif order.get_delivery_type() == '1':  # Short links
                 # Check if no other links are being generated
                 if not os.path.isfile('links.html'):
-                    print "No other short links are being generated"
-                    print "Processing order number ", order.pk, " for the activity ", order.activity
+                    print "  No other short links are being generated"
+                    print "  Processing order number ", order.pk, " for the activity ", order.activity
                     order.process(submit_link)
-                    print "Successfully generated requested short links."
+                    print timezone.now()
+                    print "  Successfully generated requested short links."
                 else:
-                    print "Other short links are currently being generated."
-                    print "Aborting..."
+                    print "  Other short links are currently being generated."
+                    print "  Aborting..."
         else:
-            print "No approved and unprocessed orders."
+            print "  No approved and unprocessed orders."

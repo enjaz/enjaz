0df785db4e8ab3d62e0568d0bdf8e7a712ea2619
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-10-31 11:34:11 +0300

    Added the restriction of having at least 1 field in any form. This prevents creation of empty forms.

--- forms_builder/wrapper/forms.py
+++ forms_builder/wrapper/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.forms.models import inlineformset_factory
+from django.forms.models import inlineformset_factory, BaseInlineFormSet
+from django.utils.translation import ungettext, ugettext_lazy as _
 from forms_builder.forms.models import Form, Field
 
 
@@ -12,5 +13,33 @@ class FormToBuildForm(forms.ModelForm):
         exclude = ('sites', 'redirect_url', 'login_required', 'send_email', 'email_from',
                    'email_copies', 'email_subject', 'email_message')
 
+
+class FieldInlineFormSet(BaseInlineFormSet):
+    """
+    Inline formset that enforces a minimum number of non-deleted forms that are not empty.
+    Enhanced version of: http://techblog.torchbox.com/post/54428118919/minimum-number-of-forms-in-a-django-formset
+    """
+    min_forms = 1
+    min_forms_message = _("The form should contain at least 1 field")
+    # min_forms_message = ungettext("The form should contain at least 1 field",
+    #                               "The form should contain at least %(count)s fields",
+    #                               min_forms) % {"count": min_forms}
+
+    def clean(self):
+        """
+        Count the actual forms that are going to be saved and make sure they exceed the minimum.
+        """
+        actual_forms = 0
+        for i in xrange(0, self.total_form_count()):
+            form = self.forms[i]
+            # Make sure form is (1) Not empty and (2) Not marked for deletion
+            if not (form.instance.id is None and not form.has_changed()) \
+                    and not self._should_delete_form(form):
+                actual_forms += 1
+        if actual_forms < self.min_forms:
+            raise forms.ValidationError(self.min_forms_message)
+
+
 # A form set to manage adding, modifying, or deleting fields of a form
-FieldFormSet = inlineformset_factory(Form, Field, exclude=('slug',), extra=1, can_delete=True)
+FieldFormSet = inlineformset_factory(Form, Field, formset=FieldInlineFormSet,
+                                     exclude=('slug',), extra=1, can_delete=True)

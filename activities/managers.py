4368d9f1ff0f852950f69d68d417bd11570b3b3e
Muhammad Saeed Arabi <msarabi95@gmail.com>
2014-12-16 20:47:57 +0300

    Implemented custom activity getter functions as a custom manager for the ``Activity`` model

---
+++ activities/managers.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class ActivityManager(models.Manager):
+    """
+    Custom manager for Activity model with custom querysets
+    for approved, pending, and rejected activities.
+    """
+    def approved(self):
+        """
+        Return a queryset of the current year's approved activities.
+        """
+        # Approved activities are those that have an approved presidency review
+        # and an approved deanship review
+        return self.filter(review__review_type="P",
+                           review__is_approved=True)\
+                   .filter(review__review_type="D",
+                           review__is_approved=True)
+
+    def pending(self):
+        """
+        Return a queryset of the current year's pending activities.
+        """
+        # Pending activities are those that are neither approved nor rejected.
+        return self.filter(review__review_type="P", review__is_approved=None).filter(review__review_type="D",
+                                                                                     review__is_approved=None)\
+            | self.filter(review__review_type="P", review__is_approved=True).filter(review__review_type="D",
+                                                                                    review__is_approved=None)\
+            | self.filter(review__review_type="P", review__is_approved=None).filter(review__review_type="D",
+                                                                                    review__is_approved=True)\
+            | self.filter(review__review_type="D").exclude(review__review_type=
+                                                           "P").exclude(review__is_approved=False)\
+            | self.filter(review__review_type="P").exclude(review__review_type=
+                                                           "D").exclude(review__is_approved=False)\
+            | self.filter(review__isnull=True)
+
+    def rejected(self):
+        """
+        Return a queryset of the current year's rejected activities.
+        """
+        # Rejected activities are those that have a rejected presidency review
+        # or a rejected deanship review
+        return self.filter(review__is_approved=False)

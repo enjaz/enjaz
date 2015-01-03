9e695ad799713bf9b323b3149cef0f3d0fddac07
Osama Khalid <osamak@gnu.org>
2015-01-03 19:08:38 +0300

    Introduce django-constance

--- core/utilities.py
+++ core/utilities.py
@@ -1,6 +1,6 @@
-from django.conf import settings
+from constance import config
 from django.contrib.auth.models import User
 
-FVP_EMAIL = User.objects.get(username=settings.FVP_USERNAME).email
-MVP_EMAIL = User.objects.get(username=settings.MVP_USERNAME).email
-DHA_EMAIL = User.objects.get(username=settings.DHA_USERNAME).email
+FVP_EMAIL = User.objects.get(username=config.FVP_USERNAME).email
+MVP_EMAIL = User.objects.get(username=config.MVP_USERNAME).email
+DHA_EMAIL = User.objects.get(username=config.DHA_USERNAME).email

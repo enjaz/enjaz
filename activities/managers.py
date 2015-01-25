218ca48ee096671480ddc5fe819eff8c7f3b9ac6
Muhammad Saeed Arabi <msarabi95@gmail.com>
2015-01-25 14:44:46 +0300

    Add manager method ``for_user`` for ``Activity`` to return the appropriate activity scope based on the passed user

--- activities/managers.py
+++ activities/managers.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.db.models.aggregates import Count
+from clubs.utils import is_coordinator_of_any_club, is_deputy_of_any_club, get_user_clubs, \
+    get_user_coordination_and_deputyships
 
 
 class ActivityManager(models.Manager):
@@ -57,3 +59,72 @@ class ActivityManager(models.Manager):
         pending_pks = [activity.pk for activity in pending_activities]
 
         return self.filter(pk__in=pending_pks)
+
+    def for_user(self, user=None):
+        """
+        Return the queryset of activities the user is allowed to see.
+        If no user is specified, return only approved activities.
+        """
+        # If user is not specified, or user is not a coordinator or deputy of any club, return only approved
+        # If user is a superuser, return everything
+        # If user is a coordinator or deputy, return (a) all the user's club's activities + (b) activities by child
+        #   and grandchild clubs that have been approved by the club's direct reviewer child (the first child that
+        #   has `can_review=True`) + (c) all activities by clubs that directly submit their activities to this
+        #   club + (d) all approved activities
+
+        if not user:
+            return self.approved()
+
+        elif user.is_superuser:
+            return self.all()
+
+        elif is_coordinator_of_any_club(user) or is_deputy_of_any_club(user):
+            clubs = get_user_coordination_and_deputyships(user)
+            a = self.filter(primary_club__in=clubs)
+
+            def get_reviewer_children(club):
+                """
+                Return a list of a club's reviewer children; ie, the clubs whose reviews go up to this club.
+                As well as a list of the club's bottom children; ie, children at the bottom of the hierarchy that
+                submit their activities to this club.
+                """
+                reviewer_children = list()
+                for child in club.children.all():
+                    # If the club can review then add it and move to the next
+                    if child.can_review:
+                        reviewer_children.append(child)
+                    else:
+                        # Otherwise, look at the children of the club
+                        reviewer_children.extend(get_reviewer_children(child))
+
+                return reviewer_children
+
+            def get_bottom_children(club):
+                """
+                Return  a list of the club's bottom children; ie, children at the bottom of the hierarchy that
+                submit their activities to this club.
+                """
+                bottom_children = list()
+                for child in club.children.all():
+                    # If the club is at the bottom of the hierarchy,
+                    # then add it and move to the next
+                    if child.children.count() == 0:
+                        bottom_children.append(child)
+                    else:
+                        # Otherwise, look at the children of the club
+                        bottom_children.extend(get_bottom_children(child))
+
+                return bottom_children
+
+            reviewer_children = set(child for club in clubs for child in get_reviewer_children(club))
+            b = self.filter(review__is_approved=True, review__reviewer_club__in=reviewer_children)
+
+            bottom_children = set(child for club in clubs for child in get_bottom_children(club))
+            c = self.filter(primary_club__in=bottom_children)
+
+            d = self.approved()
+
+            return (a | b | c | d).distinct()
+
+        else:
+            return self.approved()

a45134bdd2393409db76cec796d4557ab63484d7
Osama Khalid <osamak@gnu.org>
2015-06-14 02:24:34 +0300

    Update club admin filters.

--- clubs/admin.py
+++ clubs/admin.py
@@ -2,6 +2,23 @@
 from django.contrib import admin
 from clubs.models import Club, College, city_choices, gender_choices
 from accounts.admin import deanship_admin
+from core.models import StudentClubYear
+
+class YearFilter(admin.SimpleListFilter):
+    title = u"السنة"
+    parameter_name = 'year'
+    def lookups(self, request, model_admin):
+        years = StudentClubYear.objects.all()
+        year_lookups = [(year.start_date.year, unicode(year))
+                        for year in years]
+        return year_lookups
+
+    def queryset(self, request, queryset):
+        if self.value():
+            start_year = int(self.value())
+            end_year = start_year + 1
+            chosen_year = StudentClubYear.objects.get_by_year(start_year, end_year)
+            return queryset.filter(year=chosen_year)
 
 class ClubFilter(admin.SimpleListFilter):
     title = u"نوع النادي"
@@ -9,17 +26,17 @@ class ClubFilter(admin.SimpleListFilter):
     def lookups(self, request, model_admin):
         return (
             ('p', u'الرئاسة'),
-            ('s', u'نادي فرعي'),
+            ('s', u'نادي متخصص'),
             ('c', u'نادي كلية'),
             )
 
     def queryset(self, request, queryset):
         if self.value() == 'p':
-            return queryset.filter(english_name='Presidency')
+            return queryset.filter(english_name__icontains='Presidency')
         elif self.value() == 'c':
             return queryset.exclude(college__isnull=True)
         elif self.value() == 's':
-            return queryset.filter(college__isnull=True).exclude(pk=1)
+            return queryset.filter(college__isnull=True).exclude(english_name__icontains='Presidency')
 
 class CityFilter(admin.SimpleListFilter):
     title = u"المدينة"
@@ -42,8 +59,8 @@ class GenderFilter(admin.SimpleListFilter):
             return queryset.filter(gender=self.value())
         
 class ClubAdmin(admin.ModelAdmin):
-    list_display = ('name', 'english_name', 'email', 'coordinator', 'number_of_members')
-    list_filter = (ClubFilter, CityFilter, GenderFilter)
+    list_display = ('__unicode__', 'city', 'email', 'coordinator', 'number_of_members')
+    list_filter = (ClubFilter, CityFilter, GenderFilter, YearFilter)
     filter_horizontal = ('members', 'deputies')
 
     def number_of_members(self, obj):

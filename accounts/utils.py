4967a643ba5ffb7cb28e40181fdd849f8a10b037
Osama Khalid <osamak@gnu.org>
2015-06-17 04:23:36 +0300

    The database field doesn't accept None, so let's return an empty string.

--- accounts/utils.py
+++ accounts/utils.py
@@ -4,22 +4,22 @@ Utility functions for the accounts app.
 from django.core.exceptions import ObjectDoesNotExist
 
 def get_user_city(user):
-    """Return the user's city.  If unavailable, return None."""
-    # If either the profile is absent (i.e. superuser), return None.
+    """Return the user's city.  If unavailable, return an empty string."""
+    # If the profile is absent (i.e. superuser), return None.
     try: 
         city = user.common_profile.city
     except ObjectDoesNotExist:
-        city = None
+        city = ''
 
     return city
 
 def get_user_gender(user):
-    """Return the user's city.  If unavailable, return None."""
+    """Return the user's city.  If unavailable, return an empty string."""
     # If either the profile (i.e. superuser) or the college
-    # (i.e. non-student) are absent, return None.
+    # (i.e. non-student) are absent, return an empty string.
     try: 
         gender = user.common_profile.college.gender
     except (ObjectDoesNotExist, AttributeError):
-        gender = None
+        gender = ''
 
     return gender

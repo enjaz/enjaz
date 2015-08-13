e8d9b3c27b2ebc9e3784808b1404494046635906
Osama Khalid <osamak@gnu.org>
2015-08-13 03:57:32 +0300

    View complete name in the autocomplete box

--- niqati/autocomplete_light_registry.py
+++ niqati/autocomplete_light_registry.py
@@ -2,17 +2,20 @@
 import autocomplete_light
 from django.contrib.auth.models import User
 
-# This will generate a PersonAutocomplete class
-autocomplete_light.register(User,
-    search_fields=['^email', '^common_profile__ar_first_name',
-                             '^common_profile__ar_last_name',
-                             '^common_profile__en_first_name',
-                             '^common_profile__en_last_name'],
-    attrs={
-        'placeholder': 'أَضف طالبا',
-        'data-autocomplete-minimum-characters': 1,
-    },
-    widget_attrs={
-        'class': 'modern-style',
-    },choices=User.objects.filter(common_profile__is_student=True, coordination__isnull=True))
 
+class UserAutocomplete(autocomplete_light.AutocompleteModelTemplate):
+    search_fields=['^email', '^common_profile__ar_first_name',
+                   '^common_profile__ar_last_name',
+                   '^common_profile__en_first_name',
+                   '^common_profile__en_last_name',
+                   '^common_profile__student_id',
+                   '^common_profile__mobile_number']
+    choice_template = 'niqati/autocomplete_choice.html'
+    model = User
+    attrs = {
+         'placeholder': 'أَضف طالبا',
+         'data-autocomplete-minimum-characters': 1}
+    widget_attrs = {'class': 'modern-style'}
+    choices = User.objects.filter(common_profile__is_student=True, coordination__isnull=True)
+
+autocomplete_light.register(UserAutocomplete)

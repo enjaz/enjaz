8f1b431f97df14da6a698b9f8d789ca65dab1531
Osama Khalid <osamak@gnu.org>
2015-12-19 21:38:43 +0300

    Fix membership check

--- hpc/utils.py
+++ hpc/utils.py
@@ -1,9 +1,5 @@
-import clubs.utils
-
 def is_research_committee_member(user):
-    coordination_and_deputyships = clubs.utils.get_user_coordination_and_deputyships(user)
-    return coordination_and_deputyships.filter(english_name="Research Committee of the HPC").exists()
+    return user.memberships.current_year().filter(english_name="Research Committee of the HPC").exists()
 
 def is_organizing_committee_member(user):
-    coordination_and_deputyships = clubs.utils.get_user_coordination_and_deputyships(user)
-    return coordination_and_deputyships.filter(english_name="Organizing Committee of the HPC").exists()
+    return user.memberships.current_year().filter(english_name="Organizing Committee of the HPC").exists()

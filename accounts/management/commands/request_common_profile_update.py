1bd90d597b23f49bce3ca3402256c9bb1ad22647
Osama Khalid <osamak@gnu.org>
2016-02-05 22:28:16 +0300

    Handle missing and invalid email addresses.

--- accounts/management/commands/request_common_profile_update.py
+++ accounts/management/commands/request_common_profile_update.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 from django.core.urlresolvers import reverse
+
 
 from post_office import mail
 
@@ -14,8 +16,12 @@ class Command(BaseCommand):
         full_url = "https://{}{}".format(domain,
                                          reverse('edit_common_profile'))
         for user in User.objects.filter(common_profile__is_student=True,
-                                        is_active=True):
-            mail.send([user.email],
-                       template="update_common_profile",
-                       context={'user': user, 'full_url': full_url})
-            self.stdout.write(u'Emailed {}.'.format(user.email))
+                                        is_active=True).exclude(email=""):
+            try:
+                mail.send([user.email],
+                          template="update_common_profile",
+                          context={'user': user, 'full_url': full_url})
+                self.stdout.write(u'Emailed {}.'.format(user.email))
+            except ValidationError:
+                self.stdout.write(u'Error with {}'.format(user.email))
+            

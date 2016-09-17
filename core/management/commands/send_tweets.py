60797e8100e60393b316e1e7bb42b0cfe3bde488
Osama Khalid <osamak@gnu.org>
2016-09-17 16:31:35 +0300

    Keep track of failed tweet sending attempts

--- core/management/commands/send_tweets.py
+++ core/management/commands/send_tweets.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "Send out tweets."
 
     def handle(self, *args, **options):
-        for tweet in Tweet.objects.filter(was_sent=False):
+        for tweet in Tweet.objects.filter(was_sent=False, failed_trails__lte=5):
             user_tokens = tweet.user.social_auth.all()[0].tokens
             api = twitter.Api(consumer_key=settings.SOCIAL_AUTH_TWITTER_KEY,
                               consumer_secret=settings.SOCIAL_AUTH_TWITTER_SECRET,
@@ -21,6 +21,8 @@ class Command(BaseCommand):
                     status = api.PostUpdate(tweet.text)
             except twitter.TwitterError, e:
                 print "Something went wrong with #{}: ".format(tweet.pk), e
+                tweet.failed_trails += 1
+                tweet.save()
                 continue
 
             tweet.tweet_id = status.id

8926a39e36fecd3ab01166f8fe8396a6ee37c4f5
Zaha Jamaan <Zaha@localhost.localdomain>
2017-02-28 01:43:28 +0300

    Final CP Game

--- questions/views.py
+++ questions/views.py
@@ -9,16 +9,23 @@ from django.shortcuts import render, get_object_or_404
 
 
 
-def game_home(request):
+def show_game(request):
+    context ={}
+    if request.user.is_authenticated():
+        context['user_game'] = Game.objects.filter(user=request.user).first()
 
-    return render(request, 'the_game.html')
+    return render(request, 'the_game.html',context)
 
 
 @decorators.ajax_only
 @decorators.post_only
 @csrf.csrf_exempt
 @login_required
-def toggle_new_game(request):
+def start_new_game(request):
+
+    if request.user.is_authenticated() and Game.objects.filter(user=request.user).exists() and \
+            not request.user.is_superuser:
+        raise Exception (u"عفوا! لا يمكنك اللعب إلا مرة واحدة")
 
     game = Game.objects.create(user=request.user)
     game.save()
@@ -26,29 +33,27 @@ def toggle_new_game(request):
     results = {"game_pk": game.pk,
                "questions":[]
                }
-    for booth in Booth.objects.all():
-        questions =booth.question_set.order_by('?')
-
-        for question in questions:
-            options = question.choice_set.all()
-            choices =[]
-            text_result = []
-            for choice in options:
-                choices.append({"pk": choice.pk, "text": choice.text})
-            if question.question_type == 'Q':
-                text_result.append({"text": question.text})
-            elif question.question_type == 'F':
-                for image in question.questionfigure_set.all():
-                    text_result.append({"image":image.figure.url})
-            elif question.question_type == 'S':
-                text_result.append({"image":question.questionfigure_set.first().figure.url})
-            question_result = {"type": question.question_type,
-                               "pk": question.pk,
-                               "choices": choices,
-                               "text": question.text,
-                               "content": text_result,
-                               }
-            results["questions"].append(question_result)
+    for booth in Booth.objects.filter(question__isnull=False).distinct():
+        question = booth.question_set.order_by('?').first()
+        options = question.choice_set.all()
+        choices =[]
+        text_result = []
+        for choice in options:
+            choices.append({"pk": choice.pk, "text": choice.text})
+        if question.question_type == 'Q':
+            text_result.append({"text": question.text})
+        elif question.question_type == 'F':
+            for image in question.questionfigure_set.all():
+                text_result.append({"image":image.figure.url})
+        elif question.question_type == 'S':
+            text_result.append({"image":question.questionfigure_set.first().figure.url})
+        question_result = {"type": question.question_type,
+                            "pk": question.pk,
+                            "choices": choices,
+                            "text": question.text,
+                            "content": text_result,
+                            }
+        results["questions"].append(question_result)
 
 #            {"game_pk": game.pk,
 #             "questions": [{type: "text",
@@ -64,7 +69,7 @@ def toggle_new_game(request):
 @decorators.post_only
 @login_required
 @csrf.csrf_exempt
-def toggle_right_answer(request):
+def check_answer(request):
     choice_pk = request.POST.get('choice_pk')
     choice = get_object_or_404(Choice, pk=choice_pk)
     question_pk = request.POST.get('question_pk')
@@ -80,10 +85,10 @@ def toggle_right_answer(request):
     score = game.right_answers
     return {"right":right,"score":score}
 
-def scores (request):
-    game =Game.objects.all()
-    scores = game.order_by('-right_answers')
-    return render(request, 'the_game_score.html')
+def show_scores(request):
+    games =Game.objects.all()
+    highest_games = games.order_by('-right_answers','-submission_date')[:10]
+    return render(request, 'the_game_score.html',{"highest_games":highest_games})
 
 
 

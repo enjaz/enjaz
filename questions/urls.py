049e460379d483c533c6c1c7e20e3490ff26f914
Zaha Jamaan <Zaha@localhost.localdomain>
2017-02-27 18:57:03 +0300

    The game

--- questions/urls.py
+++ questions/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('',
                        url(r'^NewGame/$', views.game_home, name='game_home'),
                        url(r'^NewGame/start/', views.toggle_new_game,name='toggle_new_game'),
                        url(r'^NewGame/answer/', views.toggle_right_answer, name='toggle_right_answer'),
+                       url(r'^NewGame/scores/', views.scores, name='scores'),
+
                        )

ae1c6c020225423bcfaff5b3c830345467d87b2d
Osama Khalid <osamak@gnu.org>
2017-04-05 23:05:08 +0300

    Add permission check for TEDx

--- tedx/views.py
+++ tedx/views.py
@@ -1,6 +1,8 @@
-from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from clubs.models import Team
 from .forms import RegistrationForm
 from .models import Registration
 import utils
@@ -22,7 +24,15 @@ def handle_registration(request):
 
     return render(request, 'tedx/index.html', context)
 
+@login_required
 def list_registration(request):
+    tedx_team = Team.objects.get(code_name="tedx_2017_registration")
+    is_tedx_member = tedx_team.members.filter(pk=request.user.pk).exists() or\
+                     tedx_team.coordinator == request.user
+    if not request.user.is_superuser and\
+       not is_tedx_member:
+        raise PermissionDenied
+
     list_registration = Registration.objects.all()
     context = {'list_registration' : list_registration}
     return render(request, 'tedx/list_registration.html', context)

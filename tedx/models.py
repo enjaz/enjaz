365bcc0c434151dfbf8937c49eed7706fd51eefe
Osama Khalid <osamak@gnu.org>
2017-04-15 16:49:26 +0300

    Add game\! (here is to Qumaizi\!)

--- tedx/models.py
+++ tedx/models.py
@@ -46,3 +46,22 @@ class Registration(models.Model):
     your_interest = models.CharField(max_length=50, choices=interests_choices, verbose_name=u"اي المجالات التالية أقرب لاهتمامك؟",)
     submission = models.DateTimeField(u'تاريخ الإرسال', auto_now=True)
     modification = models.DateTimeField(u'تاريخ الإرسال', auto_now=True)
+
+class Game(models.Model):
+    first_question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+class Question(models.Model):
+    question_text = models.TextField(default="question text", verbose_name='نص السؤال')
+    title = models.CharField(default='question title', max_length=30)
+    def __unicode__(self):
+        return self.title
+
+class Choice (models.Model):
+    question = models.ForeignKey(Question, related_name='choices',on_delete=models.CASCADE)
+    next_question = models.ForeignKey(Question, related_name="leading_choices", on_delete=models.CASCADE, null=True)
+    choice_text = models.CharField(default='خيار', max_length=3159)
+    title = models.CharField(default='choice title', max_length=30)
+    icon = models.CharField(max_length=30, blank=True)
+    flag = models.CharField(default='choice title', max_length=30, blank=True)
+    def __unicode__(self):
+        return self.title

5af9b2bf5ae2a6ce09bf8cfbc8f41839fb94bf92
Osama Khalid <osamak@gnu.org>
2017-05-13 01:21:44 +0300

    Add certificate admin

--- certificates/admin.py
+++ certificates/admin.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8  -*-
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+from django.contrib.admin.forms import AdminAuthenticationForm
+from django.contrib.auth import authenticate
 from certificates import models
 from core.utils import BASIC_SEARCH_FIELDS
+import clubs.utils
+import events.utils
+import media.utils
+
+def has_access(user):
+    return media.utils.is_media_coordinator_or_deputy(user) or \
+        clubs.utils.is_presidency_coordinator_or_deputy(user) or \
+        events.utils.get_user_organizing_events(user).exists()  or \
+        user.is_superuser
+
+class CertificateListAuthenticationForm(AdminAuthenticationForm):
+    def confirm_login_allowed(self, user):
+        if not user.is_active and \
+           not has_access(user):
+            raise forms.ValidationError(
+                self.error_messages['invalid_login'],
+                code='invalid_login',
+                params={'username': self.username_field.verbose_name}
+                )
+
+class CertificateListAdmin(admin.sites.AdminSite):
+    login_form = CertificateListAuthenticationForm
+
+    def has_permission(self, request):
+        return has_access(request.user)
 
 class TextPositionInline(admin.TabularInline):
     model = models.TextPosition
@@ -25,6 +54,15 @@ class CertificateAdmin(admin.ModelAdmin):
     actions = [regenerate_certificate]
     inlines = [CertificateTextInline]
 
+    def has_module_permission(self, request, obj=None):
+        return has_access(request.user)
+
+    def has_change_permission(self, request, obj=None):
+        return has_access(request.user)
+
+certificate_admin = CertificateListAdmin("Certificate Admin")
+
 admin.site.register(models.Certificate, CertificateAdmin)
+certificate_admin.register(models.Certificate, CertificateAdmin)
 admin.site.register(models.CertificateRequest)
 admin.site.register(models.CertificateTemplate, TemplateAdmin)

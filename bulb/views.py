45dea784bbe090e4bbe1d59a976df879195c7035
Amjaad Abdullah <amjaad.abdullah@gmail.com>
2017-06-11 20:26:09 +0300

    limit adding books to specific times

--- bulb/views.py
+++ bulb/views.py
@@ -179,7 +179,9 @@ def order_instructions(request, pk):
     book = get_object_or_404(Book, pk=pk, is_deleted=False)
     book_exchange_coordinator = clubs.utils.get_team_for_user("book_exchange", book.submitter).coordinator
     last_request = book.last_pending_request()
-
+    current_year = StudentClubYear.objects.get_current()    
+    if current_year.bookexchange_closure_date and timezone.now().date() < current_year.bookexchange_closure_date:
+        return render(request, 'bulb/exchange/bookexchange_closure.html',  {'current_year':current_year})
     if not (last_request and last_request.requester == request.user) and \
        not utils.can_order_book(request.user, book):
         raise PermissionDenied
@@ -1862,6 +1864,6 @@ def show_recommendation_category(request, code_name):
     return render(request, "bulb/recommendations/show_category.html", context)
 
 def show_recommended_book(request, pk):
-    recommended_book = get_object_or_404(RecommendedBook, pk=pk)    
+    recommended_book = get_object_or_404(RecommendedBook, pk=pk)
     context = {'recommended_book': recommended_book}
     return render(request, "bulb/recommendations/show_recommended_book.html", context)

53de9bc254dbe08292821b2274904aa73bfff76d
Muhammad Saeed Arabi <msarabi95@gmail.com>
2017-07-30 21:17:00 +0300

    Add custom admin view to download survey results as Excel Reformat accounts/admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,29 +1,43 @@
 # -*- coding: utf-8  -*-
+from functools import update_wrapper
+
+import openpyxl
 from django import forms
+from django.conf.urls import url
 from django.contrib import admin
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext, ugettext_lazy as _
+
 from certificates.admin import certificate_admin, CertificateAdminPermission
 from ckeditor.widgets import CKEditorWidget
 from core.admin import ModelAdminReadOnly
 from core.forms import OptionalForm
 from core.utils import BASIC_SEARCH_FIELDS
+from events.models import Survey
 from . import models
 import events.forms
+
+
 
 
 def mark_deleted(modeladmin, request, queryset):
     queryset.update(is_deleted=True)
 mark_deleted.short_description = u"علم السجلات المُحدّدة أنها محذوفة (دون إزالتها من قاعدة البيانات)"
 
+
 class AbstractFigureInline(admin.TabularInline):
     model = models.AbstractFigure
     form = OptionalForm
     extra = 0
 
+
 class QuestionInline(admin.TabularInline):
     model = models.Question
     extra = 1
-    
+
+
 class InitiativeFigureInline(admin.TabularInline):
     model = models.InitiativeFigure
     extra = 0
@@ -32,11 +46,12 @@ class InitiativeFigureInline(admin.TabularInline):
 class RegistrationUniversityFilter(admin.SimpleListFilter):
     title = "University"
     parameter_name = 'university'
+
     def lookups(self, request, model_admin):
         return (
             ('ksauhs', 'KSAU-HS'),
             ('other', 'Others')
-            )
+        )
 
     def queryset(self, request, queryset):
         if self.value() == 'ksauhs':
@@ -44,15 +59,18 @@ class RegistrationUniversityFilter(admin.SimpleListFilter):
         elif self.value() == 'other':
             return queryset.filter(nonuser__isnull=False)
 
+
 class EventFilter(admin.SimpleListFilter):
     title = "Event"
     parameter_name = 'event'
+
     def lookups(self, request, model_admin):
-        return models.Event.objects.values_list('pk', 'name') 
+        return models.Event.objects.values_list('pk', 'name')
 
     def queryset(self, request, queryset):
         if self.value():
             return queryset.filter(first_priority_sessions__event__pk=self.value()).distinct()
+
 
 class RegistrationAdmin(admin.ModelAdmin):
     list_display = ['pk', '__unicode__', 'get_university', 'get_college',
@@ -67,58 +85,68 @@ class RegistrationAdmin(admin.ModelAdmin):
                                            'nonuser__ar_middle_name',
                                            'nonuser__ar_last_name']
 
+
 class RegistrationInline(admin.StackedInline):
     model = models.Registration
     extra = 1
     max_num = 1
     fields = ['sessions']
-    
+
+
 class NonUserAdmin(admin.ModelAdmin):
     list_display = ['pk', '__unicode__', 'university', 'college',
                     'with_deleted_registration', 'date_submitted']
-    search_fields= ('email',
-                    'en_first_name',
-                    'en_middle_name',
-                    'en_last_name',
-                    'ar_first_name',
-                    'ar_middle_name',
-                    'ar_last_name')
+    search_fields = ('email',
+                     'en_first_name',
+                     'en_middle_name',
+                     'en_last_name',
+                     'ar_first_name',
+                     'ar_middle_name',
+                     'ar_last_name')
 
     def with_deleted_registration(self, obj):
         return obj.hpc2016_registration.is_deleted
+
     with_deleted_registration.boolean = True
 
     inlines = [RegistrationInline]
 
+
 class AbstractPosterInline(admin.TabularInline):
-     model= models.AbstractPoster
-     form = OptionalForm
-     extra = 0
+    model = models.AbstractPoster
+    form = OptionalForm
+    extra = 0
+
 
 class SurveyQuestionInline(admin.TabularInline):
-    model= models.SurveyQuestion
+    model = models.SurveyQuestion
+
 
 class SurveyAnswerInline(CertificateAdminPermission, admin.TabularInline):
-    model= models.SurveyAnswer
+    model = models.SurveyAnswer
     exclude = ['text_value', 'numerical_value']
     readonly_fields = ['question', 'get_value']
+
 
 class AbstractAdmin(admin.ModelAdmin):
     search_fields = BASIC_SEARCH_FIELDS + ["email", "title"]
     actions = [mark_deleted]
     list_filter = ["event", "is_deleted"]
     form = events.forms.AbstractForm
-    inlines = [AbstractFigureInline,AbstractPosterInline]
+    inlines = [AbstractFigureInline, AbstractPosterInline]
     filter_horizontal = ('evaluators',)
     introduction = forms.CharField(widget=CKEditorWidget())
+
 
 class InitiativeAdmin(admin.ModelAdmin):
     inlines = [InitiativeFigureInline]
     actions = [mark_deleted]
 
+
 class SessionGroupAdmin(admin.ModelAdmin):
     list_filter = ['event']
     list_display = ['title', 'event']
+
 
 class AttendanceAdmin(admin.ModelAdmin):
     form = events.forms.AttendanceAdminForm
@@ -129,9 +157,11 @@ class AttendanceAdmin(admin.ModelAdmin):
     search_fields = ['session_registration__' + field
                      for field in BASIC_SEARCH_FIELDS]
 
+
 class EventAdmin(admin.ModelAdmin):
     list_display = search_fields = ['official_name', 'english_name',
                                     'code_name']
+
 
 class SessionAdmin(admin.ModelAdmin):
     list_filter = ['event']
@@ -140,18 +170,21 @@ class SessionAdmin(admin.ModelAdmin):
     search_fields = ['name', 'event__official_name',
                      'event__english_name']
 
+
 class SessionRegistrationAdmin(admin.ModelAdmin):
     form = events.forms.SessionRegistrationAdminForm
     actions = [mark_deleted]
-    list_display = ['__unicode__',  'is_approved', 'is_deleted',
+    list_display = ['__unicode__', 'is_approved', 'is_deleted',
                     'date_submitted']
     search_fields = BASIC_SEARCH_FIELDS + ['session__name']
     list_filter = ['session__event', 'is_deleted', 'is_approved']
+
 
 class QustionSessionAdmin(admin.ModelAdmin):
     search_fields = ['title', 'question__question_text']
     list_filter = ['event']
     inlines = [QuestionInline]
+
 
 class SurveyAdmin(CertificateAdminPermission, admin.ModelAdmin):
     search_fields = ['name', 'mandatory_sessions__event__official_name',
@@ -161,11 +194,49 @@ class SurveyAdmin(CertificateAdminPermission, admin.ModelAdmin):
     list_display = ['__unicode__', 'get_response_count']
     inlines = [SurveyQuestionInline]
 
+    def get_urls(self):
+        urlpatterns = super(SurveyAdmin, self).get_urls()
+
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            return update_wrapper(wrapper, view)
+
+        info = self.model._meta.app_label, self.model._meta.model_name
+
+        urlpatterns = [
+            url(r'^(.+)/results/$', wrap(self.results_as_excel), name='%s_%s_results' % info),
+        ] + urlpatterns
+        return urlpatterns
+
+    def results_as_excel(self, request, object_id, extra_context=None):
+        survey = get_object_or_404(Survey, id=object_id)
+        output = HttpResponse(
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8'
+        )
+        file_name = u"{} - {}".format(survey.name, _(u"النتائج"))
+        output['Content-Disposition'] = 'attachment; filename={}'.format("Survey Results.xlsx")
+
+        wb = openpyxl.Workbook()
+        ws = wb.active
+
+        # TODO: Headers should probably be excluded
+        ws.append([ugettext(u"التاريخ و الوقت"), ugettext(u"المستخدم")] + [question.text for question in survey.survey_questions.all()])
+
+        for response in survey.responses.all():
+            ws.append([response.date_submitted.strftime("%c"), response.user.username] + [answer.text_value for answer in response.answers.all()])
+
+        wb.save(output)
+
+        return output
+
+
 class SurveyResponseAdmin(CertificateAdminPermission, ModelAdminReadOnly, admin.ModelAdmin):
     search_fields = ['survey__name'] + BASIC_SEARCH_FIELDS
     list_filter = ['survey', 'session__event']
     fields = []
     inlines = [SurveyAnswerInline]
+
 
 admin.site.register(models.Event, EventAdmin)
 admin.site.register(models.Session, SessionAdmin)
--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,84 @@
+# coding=utf-8
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from events.models import Survey
+
+
+class AdminTests(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            username='testuser1',
+            email='test1@example.com',
+            password='abcd1234',
+        )
+        self.user1.is_staff = True
+        self.user1.save()
+
+        self.client.login(
+            username='testuser1',
+            password='abcd1234',
+        )
+
+        self.user2 = User.objects.create_user(
+            username='testuser2',
+            email='test2@example.com',
+            password='abcd1234',
+        )
+
+        self.user3 = User.objects.create_user(
+            username='testuser3',
+            email='test3@example.com',
+            password='abcd1234',
+        )
+
+    def test_download_survey_results_as_excel(self):
+        survey = Survey.objects.create(name="Feedback")
+        question1 = survey.survey_questions.create(
+            category='O',
+            text=u"ما هي هوايتك المفضلة؟",
+        )
+        question2 = survey.survey_questions.create(
+            category='C',
+            choices=u"لندن\nباريس",
+            text=u"إلى أين تود الذهاب في الإجازة القادمة؟",
+        )
+
+        response1 = survey.responses.create(
+            user=self.user2,
+        )
+        response1.answers.create(
+            question=question1,
+            text_value=u"الرسم",
+        )
+        response1.answers.create(
+            question=question2,
+            text_value=u"لندن",
+        )
+
+        response2 = survey.responses.create(
+            user=self.user3,
+        )
+        response2.answers.create(
+            question=question1,
+            text_value=u"السباحة",
+        )
+        response2.answers.create(
+            question=question2,
+            text_value=u"باريس",
+        )
+
+        response = self.client.get("/admin/events/survey/{}/results/".format(survey.id))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(
+            response.get('Content-Disposition'),
+            "attachment; filename={}".format("Survey Results.xlsx")
+        )
+
+        self.assertEqual(
+            response.get('Content-Type'),
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=utf-8"
+        )

ff4098598296c4b94e51e11c895393c9f8bfb671
Muhammad Saeed Arabi <msarabi95@gmail.com>
2017-08-02 16:18:46 +0300

    Organize approvals/models.py a bit and add a few more fields

--- approvals/models.py
+++ approvals/models.py
@@ -4,25 +4,77 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class ActivityRequest(models.Model):
+class AbstractRequest(models.Model):
+    submission_datetime = models.DateTimeField(
+        _(u"وقت تقديم الطلب"),
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
+
+
+class ActivityRequest(AbstractRequest):
+    # When the activity creation request is first created, this field should initially be blank. Once
+    # the request is approved, an `Activity` object is created and linked to the request via this
+    # field. For activity update requests, it should be set to the activity that is going to be
+    # updated.
+    activity = models.ForeignKey(
+        _(u"النشاط"),
+        to='activities2.Activity',
+        null=True, blank=True,
+    )
+
     name = models.CharField(_(u"اسم النشاط"), max_length=200)
     description = ckeditor.fields.RichTextField(_(u"وصف النشاط"))
 
-    is_update_request = models.BooleanField(default=False)  # Flag to distinguish update from creation requests
+    # This is a flag to distinguish update from creation requests
+    is_update_request = models.BooleanField(
+        _(u"نوع الطلب"),
+        choices=(
+            (False, _(u"طلب إضافة")),
+            (True, _(u"طلب تعديل")),
+        ),
+        default=False,
+    )
 
     class Meta:
         verbose_name = _(u"طلب نشاط")
         verbose_name_plural = _(u"طلبات أنشطة")
 
 
-class AbstractSubRequest(models.Model):
-    parent_request = models.ForeignKey(ActivityRequest, related_name='%(class)ss')
+class ActivityCancelRequest(AbstractRequest):
+    activity = models.ForeignKey(
+        _(u"النشاط"),
+        to='activities2.Activity',
+    )
+
+    class Meta:
+        verbose_name = _(u"طلب إلغاء نشاط")
+        verbose_name_plural = _(u"طلبات إلغاء أنشطة")
+
+
+class AbstractRequestAttachment(models.Model):
+    activity_request = models.ForeignKey(
+        _(u"طلب النشاط"),
+        to='approvals.ActivityRequest',
+        related_name='%(class)ss'
+    )
 
     class Meta:
         abstract = True
 
 
-class EventSubRequest(AbstractSubRequest):
+class DescriptionField(AbstractRequestAttachment):
+    label = models.CharField(_(u"الوصف"), max_length=50)
+    value = models.CharField(_(u"القيمة"), max_length=200)
+
+    class Meta:
+        verbose_name = _(u"حقل وصفي")
+        verbose_name_plural = _(u"حقول وصفية")
+
+
+class EventSubRequest(AbstractRequestAttachment):
     label = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     date = models.DateField()
@@ -33,10 +85,6 @@ class EventSubRequest(AbstractSubRequest):
     class Meta:
         verbose_name = _(u"طلب فعالية")
         verbose_name_plural = _(u"طلبات فعاليات")
-
-
-class ActivityCancelRequest(models.Model):
-    pass
 
 
 class RequestThread(object):

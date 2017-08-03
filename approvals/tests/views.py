f6d2468603d60f0f402ecf9d87f79ee72661399f
Muhammad Saeed Arabi <msarabi95@gmail.com>
2017-08-03 15:09:10 +0300

    Test placeholders for newly created view

---
+++ approvals/tests/views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class TestSubmitActivityCreateRequest(TestCase):
+    def test_get_context_data(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
+
+    def test_post(self):
+        self.fail()

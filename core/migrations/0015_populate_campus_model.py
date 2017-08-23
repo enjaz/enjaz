209849d81550b185fdafed61207c4466424cb6b3
Muhammad Saeed Arabi <muhammadsaeed_express@yahoo.com>
2017-08-23 14:58:44 +0300

    Initial empty migrations

---
+++ core/migrations/0015_populate_campus_model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0014_auto_20170815_1006'),
+    ]
+
+    operations = [
+    ]
---
+++ core/migrations/0017_populate_college_model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0016_populate_section_model'),
+    ]
+
+    operations = [
+    ]

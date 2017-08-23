c2a723e6121bfa227109e890a01a2f85849df35d
aalsalim <a3alsalim@gmail.com>
2017-08-23 11:19:56 +0300

    Add slug to teams

--- teams/urls.py
+++ teams/urls.py
@@ -5,8 +5,9 @@ from teams import views as teams_views
 urlpatterns = [
     url(r'^$', teams_views.ListView.as_view(), name='list_teams'),
     url(r'^create/$', login_required(teams_views.CreateView.as_view()), name='create'),
-    url(r'^(?P<team_id>\d+)/$', teams_views.DetailView.as_view(), name='show'),
-    url(r'^(?P<team_id>\d+)/edit/$', login_required(teams_views.UpdateView.as_view()), name='edit'),
-    url(r'^(?P<team_id>\d+)/send_email', teams_views.send_email, name='send_email'),
-    url(r'^(?P<team_id>\d+)/ajax/add', teams_views.add_members, name='add_members'),
+    url(r'^(?P<code_name>[\d\w_\-]+)/$', login_required(teams_views.DetailView.as_view()), name='show'),
+    url(r'^(?P<year>\d{4})/(?P<code_name>[\d\w_\-]+)/$', login_required(teams_views.ArchiveDetailView.as_view()), name='archive'),
+    url(r'^(?P<code_name>[\d\w_\-]+)/edit/$', login_required(teams_views.UpdateView.as_view()), name='edit'),
+    url(r'^(?P<code_name>[\d\w_\-]+)/send_email', teams_views.send_email, name='send_email'),
+    url(r'^(?P<code_name>[\d\w_\-]+)/ajax/add', teams_views.add_members, name='add_members'),
 ]

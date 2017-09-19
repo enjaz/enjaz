bea543b866a32b03f56317944c1a318cb8552e3d
Zaha <zahajamaan@gmail.com>
2017-09-19 16:37:57 +0300

    missing_migration

---
+++ teams/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('teams', '0007_auto_20170901_0524'),
+        ('teams', '0010_merge'),
+    ]
+
+    operations = [
+    ]

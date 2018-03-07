2c541882b025e15da18babf2f7263ca255f49dfc
AbdulrahmanSami <abo55od@hotmail.com>
2018-03-07 16:42:44 +0300

    matching program migrations fix.

--- matching_program/migrations/0006_auto_20180303_0121.py
+++ matching_program/migrations/0006_auto_20180303_0121.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('matching_program', '0005_auto_20180210_1507'),
+        ('matching_program', '0001_initial'),
     ]
 
     operations = [

0ec16bfdb5c1ec33c14a27c3697bdb8531624a1f
unknown <salkhairy@hotmail.com>
2019-10-14 22:19:26 +0100

    add_forms

--- science_olympiad/urls.py
+++ science_olympiad/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url, include
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -6,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^form/$', views.view_form, name="view_form"),
     url(r'^section/$', views.view_section, name="view_section"),
+    url(r'^hackathon/form/$', TemplateView.as_view(template_name='science_olympiad/hackathon_form.html'), name='hackathon_form'),
+    url(r'^gallery/form/$', TemplateView.as_view(template_name='science_olympiad/gallery_form.html'), name='gallery_form'),
 ]

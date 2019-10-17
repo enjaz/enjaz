56ce01cfd4ae89508a7c6d92cda952c30b41f8a9
Qumayzi <abo55od@hotmail.com>
2019-10-17 22:34:03 +0300

    Temporary Research Guidelines Website

--- newhpc/urls.py
+++ newhpc/urls.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    url(r'^riyadh/ar/$', views.riy_ar_index, name="riy_ar_index"),
-    url(r'^riyadh/en/$', views.riy_en_index, name="riy_en_index"),
+    #url(r'^riyadh/ar/$', views.riy_ar_index, name="riy_ar_index"),
+    #url(r'^riyadh/en/$', views.riy_en_index, name="riy_en_index"),
     url(r'^riyadh/en/research$', views.riy_en_research, name="riy_ar_research"),
 
 ]

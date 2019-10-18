6e0e0882558ff3be810490149af6306339bd93ee
Qumayzi <abo55od@hotmail.com>
2019-10-18 12:57:12 +0300

    Abstract submission Website hotfix

--- newhpc/urls.py
+++ newhpc/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     #url(r'^riyadh/ar/$', views.riy_ar_index, name="riy_ar_index"),
     #url(r'^riyadh/en/$', views.riy_en_index, name="riy_en_index"),
-    url(r'^riyadh/en/research$', views.riy_en_research, name="riy_ar_research"),
+    url(r'^riyadh/en/research$', views.riy_en_research, name="riy_en_research"),
 
 ]
 

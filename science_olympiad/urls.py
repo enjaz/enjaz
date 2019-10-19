a9f38e00120fe1d46dabe0f4392dcb857898fa9b
unknown <salkhairy@hotmail.com>
2019-10-19 21:02:35 +0100

    merge sci_olymp

--- science_olympiad/urls.py
+++ science_olympiad/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^form/$', views.view_form, name="view_form"),
-    url(r'^section/$', views.view_section, name="view_section"),
+    url(r'^(?P<section>\w+)/$', views.view_section, name="view_section"),
     url(r'^hackathon/form/$', TemplateView.as_view(template_name='science_olympiad/hackathon_form.html'), name='hackathon_form'),
     url(r'^gallery/form/$', TemplateView.as_view(template_name='science_olympiad/gallery_form.html'), name='gallery_form'),
 ]

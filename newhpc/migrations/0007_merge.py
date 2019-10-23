c771be33ed8083319375fb226ffa27ad95f9a23c
Enjaz Portal Team <hello@enjazportal.com>
2019-10-23 15:15:24 +0000

    merge migrations

---
+++ newhpc/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('newhpc', '0005_fix_rank_hpc_winner'),
+        ('newhpc', '0006_title_n_year'),
+    ]
+
+    operations = [
+    ]

ecf79316da96ab305910efaf078154d20389cb34
unknown <salkhairy@hotmail.com>
2019-11-09 18:27:48 +0000

    hide all pages execpt inventors form

--- science_olympiad/urls.py
+++ science_olympiad/urls.py
@@ -4,12 +4,12 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
+    # url(r'^$', views.index, name="index"),
     # url(r'^form/$', views.view_form, name="view_form"),
-    url(r'^(?P<section>\w+)/$', views.view_section, name="view_section"),
+    # url(r'^(?P<section>\w+)/$', views.view_section, name="view_section"),
     # url(r'^hackathon/form/$', TemplateView.as_view(template_name='science_olympiad/hackathon_form.html'), name='hackathon_form'),
     # url(r'^gallery/form/$', TemplateView.as_view(template_name='science_olympiad/gallery_form.html'), name='gallery_form'),
     # url(r'^inventors/add/$', views.add_inventor, name='add_inventor'),
-    url(r'^inventors/add/$', views.add_inventor2, name='add_inventor'),
+    url(r'^inventors/register/$', views.add_inventor2, name='add_inventor'),
     # url(r'^inventors/add/3/$', views.add_inventor3, name='add_inventor2'),
 ]
